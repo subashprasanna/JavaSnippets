e3631dfc76b6d330b4640f1c45586db0eef6ea00
prasanna <prasanna.balthasar@idsnext.com>
2017-07-11 18:09:49 +0530

    kept comments

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,6 +3,7 @@ public class BinarySearch {
 	public BinarySearch(){}
 	
 	public static void main(String[] args) {
+		// array should be in sorted order for binary search
 		int[] mArgs = {1,3,6,9,11,15,17,20};
 		BinarySearch b = new BinarySearch();
 		if(b.findValue(mArgs, 20)) {
@@ -21,25 +22,30 @@ public class BinarySearch {
 		
 		boolean found = false;
 		while(!found) {
+			// once start & end cross each other, search lifecycle ends
 			if(end < start) {
 				found = true;
 				break;
 			}
 			
+			// Find middle index in btw start and end index
 			middle = start + (end - start) / 2;
 			
-			if(findValue < value[middle] ) {
-				end = middle - 1;
-			}
-			
-			if(findValue > value[middle]) {
-				start = middle + 1;
-			}
-			
+			// if middle index value is searchable value, we found 
 			if(value[middle] == findValue) {
 				result = true;
 				found = true;
 				break;
+			}
+			
+			// if search value < middle index array value, searchable value will be in left side only from middle, so ignore right side
+			if(findValue < value[middle] ) {
+				end = middle - 1; // limit the array search only left side
+			}
+			
+			// if search value > middle index array value, searchable value will be in right side only from middle, so ignore left side
+			if(findValue > value[middle]) {
+				start = middle + 1; // limit the array search only right side
 			}
 		}
 		
